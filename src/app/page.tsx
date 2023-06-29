dfefd75b6e6aa4a800a79de7eacdd493ada49c4b
cthacker-udel <cthacker@udel.edu>
2023-06-29 03:49:06 -0400

    [master] Added proper metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,1 +1,30 @@
+import type { Metadata } from "next";
+
+import icon from "@/assets/icons/homepage.ico";
+
+export const metadata: Metadata = {
+    applicationName: "PR-Tracker",
+    authors: [
+        { name: "cthacker-udel", url: "https://github.com/cthacker-udel" },
+    ],
+    creator: "Cameron Thacker",
+    description:
+        "Welcome to the PR tracker app! This helps you log your PR's while going to the gym, or generally any activity. It has graphs that process the data entered and feed it into an algorithm that predicts the next accomplishments!",
+    icons: icon.src,
+    keywords: [
+        "react",
+        "nextjs",
+        "workout app",
+        "workout",
+        "fitness",
+        "workout tracker",
+        "pr tracker",
+        "pr progress",
+        "gym app",
+        "gym",
+    ],
+    publisher: "Vercel",
+    title: "PR Tracker",
+};
+
 export { default } from "@/components/HomePage/HomePage";
